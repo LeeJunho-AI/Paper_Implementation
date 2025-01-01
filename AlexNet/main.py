a5b4e284a9710a8da2795ac13a193ccbfe863ce5
LeeJunho <leejunhoai0104@gmail.com>
2025-01-01 14:08:47 +0900

    create

--- AlexNet/main.py
+++ AlexNet/main.py
@@ -5,13 +5,10 @@ from evaluate import evaluate_model
 from dataset import get_dataset, get_transforms, get_dataloader
 
 def main():
-    # 디바이스 설정 (GPU 또는 CPU)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # 모델 초기화
     model = AlexNet().to(device)
 
-    # 데이터 전처리 및 로더 설정
     train_transforms = get_transforms(train=True)
     test_transforms = get_transforms(train=False)
 
@@ -24,14 +21,12 @@ def main():
     learning_rate = 0.001
     training_epochs = 1
     for mode in ["train", "evaluate"]:
-        # 실행 모드에 따른 동작
         if mode == "train":
-            print("학습을 시작합니다...")
-            # 모델 학습
+            print("Train Started...")
             train_model(model, train_loader, device, learning_rate=learning_rate, training_epochs=training_epochs)
     
         elif mode == "evaluate":
-            print("평가를 시작합니다...")
+            print("Evaluation Started...")
     
             # 모델 평가
             evaluate_model(model, test_loader, device)
